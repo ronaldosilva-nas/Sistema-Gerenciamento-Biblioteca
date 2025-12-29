cf47c389d059e8151cf6fbc609d138fbc36f01e5
Ronaldo Silva Nascimento <silvaronaldo7662@gmail.com>
2025-12-29 19:10:35 -0300

    Primeiro commit - criação do menu principal do sistema

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int op = 0;
+        
+        // Menu principal do sistema
+        do {
+            System.out.println("BEM VINDO AO SISTEMA DE GERENCIAMENTO DE BIBLIOTECA");
+            System.out.println("================ Menu Principal ================");
+            System.out.println("1 - Administração");
+            System.out.println("2 - Usuário");
+            System.out.println("3 - Encerrar");
+            System.out.println("Informe a opção desejada ou digite (3) para encerrar");
+            op = scan.nextInt();
+
+            switch (op) {
+                case 1:
+                    System.out.println("Entrou na opção 1, Administração");
+                    break;
+
+                case 2:
+                    System.out.println("Entrou na opção 2, Usuário");
+                    break; 
+            }
+
+            scan.close();
+        } 
+        while (op == 3);   
+    }
+}
