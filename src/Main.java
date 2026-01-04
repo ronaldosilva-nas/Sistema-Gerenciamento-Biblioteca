18baab06e8b7768f5d853adf43f9b1baaf11800c
Ronaldo Silva Nascimento <silvaronaldo7662@gmail.com>
2026-01-04 13:14:53 -0300

    Criando um submenu de opções da administração

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,47 @@ public class Main {
 
             switch (op) {
                 case 1:
-                    System.out.println("Entrou na opção 1, Administração");
+                    
+                int opAdmin = 0;
+
+                    // Submenu de opções da administração
+                    do {
+                        System.out.println("================ Menu administração ================");
+                        System.out.println("1 - Cadastrar livro");
+                        System.out.println("2 - Cadastrar usuário");
+                        System.out.println("3 - Pesquisar usuário");
+                        System.out.println("4 - Listar empréstimos");
+                        System.out.println("5 - Relatórios");
+                        System.out.println("6 - Busca avançada");
+                        System.out.println("7 - Sair");
+                        System.out.println("Informe a opção desejada ou digite (7) para sair");
+                        opAdmin = scan.nextInt();
+
+                        switch(opAdmin) {
+                            case 1:
+                                System.out.println("Livro cadastrado");
+                                break;
+                            case 2:
+                                System.out.println("Usuário cadastrado");
+                                break;
+                            case 3: 
+                                System.out.println("Pesquisando usuário");
+                                break;
+                            case 4: 
+                                System.out.println("Listando empréstimos");
+                                break;
+                            case 5: 
+                                System.out.println("Exibindo relatório");
+                                break;
+                            case 6: 
+                                System.out.println("Realizando busca avançada");
+                                break;                    
+                        }
+
+                    }
+                    while(opAdmin != 7);
+
+
                     break;
 
                 case 2:
